7e5a8dcb45777c6b6a4752ad3476192f53736836
Anthony Kim <fbssaejun@gmail.com>
2021-08-02 22:44:17 +0000

    added get request to redirect users to shortened url

--- express-server.js
+++ express-server.js
@@ -5,14 +5,14 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-function generateRandomString() {
+const generateRandomString = (length) => {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for ( var i = 0; i < 6; i++ ) {
+  for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
- return result;
-}
+  return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,8 +20,11 @@ const urlDatabase = {
 };
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  
-  res.send("Ok");     
+  const shortURL = generateRandomString(6);
+  const longURL = req.body["longURL"];
+  urlDatabase[shortURL] = longURL;
+  console.log(req.body);
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/", (req, res) => {
@@ -31,7 +34,8 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -40,7 +44,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL : urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-})
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
